e4f5896e518aeef7c4c3a8bb06d0251ec8d9b998
MuhammadRizky1023 <goldroger99909@gmail.com>
2024-06-06 15:37:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Home/Navbar";
 import Home from "./Pages/Home/Homescreen";
-
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/myportofolio">
         <div>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="*" element={<div>404 Not Found</div>}></Route>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<div>404 Not Found</div>} />
           </Routes>
         </div>
       </Router>
     </div>
   );
 }
-
 export default App;
